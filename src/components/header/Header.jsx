b1f0dc088ec7a2d6138cf172a902d3a59c4d072a
asM0d <112691764+asM0d@users.noreply.github.com>
2022-11-19 16:02:46 +0300

    last

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,48 +5,45 @@ import whatsapp from "../../images/whatsapp.svg";
 import "./Header.css";
 import { useWindowSize } from "../../window/useWindowSize";
 
-
 export default function Header() {
   const width = useWindowSize();
+  const maxSize = 500;
 
   return (
-
     <header>
-      {width > 500 ? (
-          <div className="bHeader">
-            <div className="header">
-              <div className="header__logo-container">
-                <img className="header__logo" src={logo} alt="логотип" />
-                <p className="header__logo-text">
-                  крупный интегратор CRM в России и еще 8 странах
-                </p>
-              </div>
+      {width > maxSize ? (
+        <div className="bHeader">
+          <div className="header">
+            <div className="header__logo-container">
+              <img className="header__logo" src={logo} alt="логотип" />
+              <p className="header__logo-text">
+                крупный интегратор CRM в России и еще 8 странах
+              </p>
+            </div>
 
-              <ul className="header__navigation">
-                <li className="header__navigation-item">Услуги</li>
-                <li className="header__navigation-item">Виджеты</li>
-                <li className="header__navigation-item">Интеграции</li>
-                <li className="header__navigation-item">Кейсы</li>
-                <li className="header__navigation-item">Сертификаты</li>
+            <ul className="header__navigation">
+              <li className="header__navigation-item">Услуги</li>
+              <li className="header__navigation-item">Виджеты</li>
+              <li className="header__navigation-item">Интеграции</li>
+              <li className="header__navigation-item">Кейсы</li>
+              <li className="header__navigation-item">Сертификаты</li>
+            </ul>
+            <div className="header__contacts">
+              <p className="header__phone">+7 555 555-55-55</p>
+              <ul className="header__contacts-list">
+                <li className="header__contacts-item">
+                  <img src={telegram} alt="telegram, телеграмм" />
+                </li>
+                <li className="header__contacts-item">
+                  <img src={viber} alt="viber, вайбер" />
+                </li>
+                <li className="header__contacts-item">
+                  <img src={whatsapp} alt="whatsapp, ватсапп" />
+                </li>
               </ul>
-              {/*1024*/}
-              <div className="header__contacts">
-                <p className="header__phone">+7 555 555-55-55</p>
-                <ul className="header__contacts-list">
-                  <li className="header__contacts-item">
-                    <img src={telegram} alt="telegram, телеграмм" />
-                  </li>
-                  <li className="header__contacts-item">
-                    <img src={viber} alt="viber, вайбер" />
-                  </li>
-                  <li className="header__contacts-item">
-                    <img src={whatsapp} alt="whatsapp, ватсапп" />
-                  </li>
-                </ul>
-              </div>
             </div>
           </div>
-
+        </div>
       ) : (
         <ul className="header__mobile">
           <li className="header__mobile-item">Услуги</li>
--- task_1_Timer/timer.js
+++ task_1_Timer/timer.js
@@ -13,50 +13,64 @@ var eventDate,
   sec,
   days;
 
+const form = document.querySelector("form");
+const formSubmit = document.querySelector("button");
+
+form.addEventListener("change", changeFormHandler);
+
+function changeFormHandler() {
+  console.log(form.checkValidity());
+  if (form.checkValidity()) {
+    formSubmit.removeAttribute("disabled");
+  }
+}
+
 function setEvent() {
   currentTime();
 }
 
 function currentTime() {
   userYear = document.getElementById("inputYear").value;
-//   console.log(userYear);
+  //   console.log(userYear);
   userMonth = document.getElementById("inputMonth").value;
-//   console.log(userMonth);
+  //   console.log(userMonth);
   userDate = document.getElementById("inputDate").value;
-//   console.log(userDate);
+  //   console.log(userDate);
   userHours = document.getElementById("inputHours").value;
-//   console.log(userHours);
+  //   console.log(userHours);
   userMinutes = document.getElementById("inputMinutes").value;
-//   console.log(userMinutes);
-   userSeconds = 0;
-   userMil = 0;
+  //   console.log(userMinutes);
+  userSeconds = 0;
+  userMil = 0;
 
-  
-  eventDate = new Date(userYear, (userMonth - 1), userDate, userHours, userMinutes, userSeconds, userMil);
+  eventDate = new Date(
+    userYear,
+    userMonth - 1,
+    userDate,
+    userHours,
+    userMinutes,
+    userSeconds,
+    userMil
+  );
   // console.log(eventDate);
-
 
   currentDate = new Date();
   // console.log(currentDate);
 
-  
   diff = eventDate - currentDate;
   // console.log(diff);
 
-  
   hours = Math.floor((diff % (60 * 60 * 24 * 1000)) / (60 * 60 * 1000));
   mins = Math.floor((diff % (60 * 60 * 1000)) / (60 * 1000));
   sec = Math.floor((diff % (60 * 1000)) / 1000);
   days = Math.floor(diff / (60 * 60 * 24 * 1000));
   // console.log(days, hours, mins, sec);
 
-  
   hours = hours < 10 ? "0" + hours : hours;
   days = days < 10 ? "0" + days : days;
   mins = mins < 10 ? "0" + mins : mins;
   sec = sec < 10 ? "0" + sec : sec;
 
-  
   document.getElementById("outputDays").textContent = days;
   document.getElementById("outputHours").textContent = hours;
   document.getElementById("outputMins").textContent = mins;
